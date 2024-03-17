fe97aafa68b4d75206f29de4142a121b33da9c05
Harsh Chafle <157059028+Harshchafle@users.noreply.github.com>
2024-03-17 03:44:53 +0000

    The method showData is added in child Class

--- AbstractClasses.java
+++ AbstractClasses.java
@@ -1,9 +1,13 @@
 public abstract class AbstractClasses {
+
     abstract void display();
+    abstract void showData();
+    
     public static void main(String args[]){
         // AbstractClasses obj1 = new AbstractClasses(); // error: AbstractClasses is abstract; cannot be instantiated
         ExtendAbstract obj1 = new ExtendAbstract();
         obj1.display();
+        obj1.showData();
     }
 }
 
@@ -11,7 +15,13 @@ class ExtendAbstract extends AbstractClasses{
     void display(){
         System.out.println("hello this is abstract class");
     }
+
+    void showData(){
+        System.out.println("This is showData method in Extended class of Abstract Class");
+    }
 }
+
+
 
 
 
